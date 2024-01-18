608ab990f3764473fe95177a49475f699f81f7a6
Mart <marta.nikiel@yahoo.com>
2024-01-18 09:29:29 +0100

    in progress

--- contacts.js
+++ contacts.js
@@ -1,66 +1,45 @@
-const fs = require("fs").promises;
-const { nanoid } = require("nanoid");
+const fs = require("fs/promises");
 const path = require("path");
-require("colors");
+const { v4 } = require("uuid");
 
-const contactsPath = path.join(__dirname, "db", "contacts.json");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-const listContacts = async () => {
-  try {
-    const contacts = await fs.readFile(contactsPath, { encoding: "utf-8" });
-    return JSON.parse(contacts);
-  } catch (error) {
-    console.log(`Error: ${error.message}`.red);
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contactIdToString = String(contactId);
+  const contactByID = contacts.find(
+    (contact) => contact.id === contactIdToString
+  );
+  if (!contactByID) {
+    return null;
   }
-};
+  return contactByID;
+}
 
-const getContactById = async (contactId) => {
-  try {
-    const contacts = await listContacts();
-    return contacts.filter(({ id }) => id === contactId);
-  } catch (error) {
-    console.log(`Error: ${error.message}`.red);
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contactIdToString = String(contactId);
+  const index = contacts.findIndex((item) => item.id === contactIdToString);
+  if (index === -1) {
+    return null;
   }
-};
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
 
-const removeContact = async (contactId) => {
-  try {
-    const contacts = await listContacts();
-    const newContacts = contacts.filter(({ id }) => id !== contactId);
-    await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2), {
-      encoding: "utf-8",
-    });
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: v4(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
 
-    return newContacts;
-  } catch (error) {
-    console.log(`Error: ${error.message}`.red);
-  }
-};
-
-const addContact = async (name, email, phone) => {
-  try {
-    const contacts = await listContacts();
-    const newContact = {
-      id: nanoid(),
-      name,
-      email,
-      phone,
-    };
-    const updatedContacts = [newContact, ...contacts];
-    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2), {
-      encoding: "utf-8",
-    });
-    return newContact;
-  } catch (error) {
-    console.log(`Error: ${error.message}`.red);
-  }
-};
-
-listContacts();
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-};
+module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,48 +1,39 @@
-const { program } = require("commander");
+const contacts = require("./contacts");
 
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-} = require("./contacts");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
 
-program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
-
-program.parse(process.argv);
-
-const argv = program.opts();
-
-const invokeAction = async ({ action, id, name, email, phone }) => {
+async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      const contacts = await listContacts();
-      console.table(contacts);
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
       break;
 
     case "get":
-      const contact = await getContactById(id);
-      console.table(contact);
+      const contact = await contacts.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
       break;
 
     case "add":
-      const newContact = await addContact(name, email, phone);
-      console.table(newContact);
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
       break;
 
     case "remove":
-      const remContact = await removeContact(id);
-      console.table(remContact);
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
       break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
-};
+}
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
 
 invokeAction(argv);
